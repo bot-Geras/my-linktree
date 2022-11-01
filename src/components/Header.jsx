d4d41b12e5a669a2403c5c01cc742879796e1868
Brian <briandave771@gmail.com>
2022-11-01 23:44:06 +0300

    Style the header mage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 export default function Header() {
   return (
     <header>
-      <img className="share-btn" src="../profile-img.png" alt="" />
+      <img className="share-btn" src="/profile-img.png" alt="" />
 
-      <img className="header-img" src="/profile__img.png" />
+      <img className="header-img" alt="" />
       <h2 className="header-title">Brian David</h2>
     </header>
   );
